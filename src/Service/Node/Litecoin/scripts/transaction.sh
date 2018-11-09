7d24a386675b202855252a1f0b80cf28658cf526
Dinozor <dino@up.biz>
2018-11-09 22:51:25 +0100

    Update src/Service/Node/Litecoin/scripts/transaction.sh, src/Service/Node/Litecoin/scripts/block.sh files

--- src/Service/Node/Litecoin/scripts/transaction.sh
+++ src/Service/Node/Litecoin/scripts/transaction.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SOURCE="${BASH_SOURCE[0]}"
-CON=$SOURCE/../../../../bin/console
+CON="$( dirname $SOURCE )/../../../../../"
 echo $CON $1 >> /tmp/ltc_transactions.txt
-php $CON node:update ltc wallet $1 &>> /tmp/ltc_transactions.txt
+cd $CON
+php bin/console node:update ltc wallet $1 &>> /tmp/ltc_transactions.txt
