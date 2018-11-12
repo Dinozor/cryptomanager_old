3aa98ad8b9cbc0db9fc2d4772e61e50f49ce1920
Dinozor <dino@mb.org>
2018-11-12 12:30:49 +0000

    Add logtime to scripts, add execute permission to scripts

--- src/Service/Node/Litecoin/scripts/transaction.sh
+++ src/Service/Node/Litecoin/scripts/transaction.sh
@@ -2,6 +2,6 @@
 
 SOURCE="${BASH_SOURCE[0]}"
 CON="$( dirname $SOURCE )/../../../../../"
-echo $CON $1 >> /tmp/ltc_transactions.txt
+echo $(date -Iseconds): $CON $1 >> /tmp/ltc_transactions.txt
 cd $CON
 php bin/console node:update ltc wallet $1 &>> /tmp/ltc_transactions.txt
